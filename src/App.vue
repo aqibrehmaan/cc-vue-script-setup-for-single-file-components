7c1509e5f19d39a88dfef7a5f4ca32d316c62872
Aqib <aqibr777@gmail.com>
2022-09-02 20:14:42 +0500

    3-the-basics

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,14 @@
 <template>
-    Hello
+    {{ authenticated }}
+    <button v-on:click="meow">Meow</button>
 </template>
 
+<script setup>
+  import { ref } from 'vue';
+
+  const authenticated = ref(false);
+  const meow = () => {
+     authenticated.value = true;
+  }
+
+</script>
